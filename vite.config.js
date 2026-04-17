24fa80296da0f89870ca1942e10b8a145026fd33
basiaszafraniec <basia.szafraniec@gmail.com>
2026-04-17 23:26:26 +0200

    omfg never again chose a name u cant spell

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/achievly/', // ← your repo name here
+  base: '/achively/', // ← your repo name here
 })
